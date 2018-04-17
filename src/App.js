1136e8ddd0524c8effff66e11114461f9049657d
Hjalti Kolbeinsson <hjalti@kolbeinsson.se>
2018-04-17 23:01:43 +0000

    :wrench::heavy_plus_sign::lipstick: Added SASS loaders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styles from './App.scss'
 import './Backend/index.js'
 import './Backend/calc2.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={styles.app}>
+        <h1>Cube Trainer</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default App
